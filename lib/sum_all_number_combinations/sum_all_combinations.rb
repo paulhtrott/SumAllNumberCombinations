4bca46ef612ba638df5e3afc5b658f1e07be3270
Paul Trott <paultrott78@gmail.com>
2016-08-20 15:48:35 -0500

    updated initialize and original attribute setter

--- lib/sum_all_number_combinations/sum_all_combinations.rb
+++ lib/sum_all_number_combinations/sum_all_combinations.rb
@@ -6,12 +6,12 @@ class SumAllCombinations
   
   def initialize(array_of_nums)
     @original = array_of_nums unless !array_of_nums.respond_to?('each')
-    @flattened = array_of_nums.flatten unless !array_of_nums.respond_to?('each')
+    @flattened = @original.flatten unless @original.nil?
   end
 
   def original=(new_array)
     @original = new_array unless !new_array.respond_to?('each')
-    @flattened = new_array.flatten unless !new_array.respond_to?('each')
+    @flattened = @original.flatten unless @original.nil?
   end
   
   def sum(remove_duplicates=false)
