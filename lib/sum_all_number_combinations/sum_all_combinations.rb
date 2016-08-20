5b7d36036854528d94649f92b5121fa75ce13d7c
Paul Trott <paultrott78@gmail.com>
2016-08-20 18:40:31 -0500

    Added sum method to the sum all combinations class

--- lib/sum_all_number_combinations/sum_all_combinations.rb
+++ lib/sum_all_number_combinations/sum_all_combinations.rb
@@ -3,7 +3,8 @@ class SumAllCombinations
   attr_accessor :original
   attr_reader :flattened
   attr_reader :calculated_values
-  
+  attr_reader :combinations_used 
+ 
   def initialize(array_of_nums)
     @original = array_of_nums unless !array_of_nums.respond_to?('each')
     @flattened = @original.flatten unless @original.nil?
@@ -14,12 +15,21 @@ class SumAllCombinations
     @flattened = @original.flatten unless @original.nil?
   end
   
-  def sum(remove_duplicates=false)
-    
+  def sum(remove_duplicates=false, sort=true)
+    @combinations_used = []
+    @calculated_values = []
+    empty_array = []
+    calculate(empty_array, @flattened, 0, 0) if !@flattened.nil?
+    @calculated_values.uniq! if remove_duplicates
+    @calculated_values.sort! if sort
   end
 
-  def remove_duplicates
-
-  end 
-  
+  private
+  def calculate(current, array_to_use, index, accumulation)
+    (index...array_to_use.length).each do |i|
+      calculate(current + [array_to_use[i]], array_to_use, i + 1, accumulation + array_to_use[i])
+    end
+   @combinations_used.push("#{current.join('+')}=#{accumulation}") if current.size > 1
+   @calculated_values.push(accumulation) if current.size > 1
+  end  
 end
--- spec/test_sum_all_combinations_spec.rb
+++ spec/test_sum_all_combinations_spec.rb
@@ -1,16 +1,16 @@
 describe SumAllCombinations do
   context "initializer" do
-    it "original_array attribute should be empty array when initialized with an empty array" do
+    it "original array attribute should be empty array when initialized with an empty array" do
       nums = SumAllCombinations.new([])
       expect(nums.original).to eq([])
     end
     
-    it "original_array attribute should be equal to array passed in" do
+    it "original array attribute should be equal to array passed in" do
       nums = SumAllCombinations.new(['house', 'car', 1, 2])
       expect(nums.original).to eq(['house', 'car', 1, 2])
     end
 
-    it "original_array attribute should be nil if array not passed in" do
+    it "original array attribute should be nil if array not passed in" do
       nums = SumAllCombinations.new('house')
       expect(nums.original).to eq(nil)
 
@@ -19,7 +19,7 @@ describe SumAllCombinations do
     end
   end
 
-  context "accessor of original_array" do
+  context "accessor of original array" do
     it "original_array should equal value it is set to" do
       nums = SumAllCombinations.new(nil)
       expect(nums.original).to eq(nil)
@@ -29,7 +29,7 @@ describe SumAllCombinations do
     end
   end
 
-  context "original multi-dimensional arrays will be flattened" do
+  context "multi-dimensional arrays" do
     it "original multi-dimension array should be flattened" do
       nums = SumAllCombinations.new([2,[3,4],5,[6,7,8,9,10]])
       expect(nums.original).to eq([2,[3,4],5,[6,7,8,9,10]])
@@ -40,4 +40,67 @@ describe SumAllCombinations do
       expect(nums.flattened).to eq([11,12,13,14,15,16,17,18,19])
     end
   end
+
+  context "sum" do
+    it "should calculate the sum of all combinations of array values" do
+      nums = SumAllCombinations.new([1,2,3])
+      nums.sum
+      expect(nums.calculated_values).to eq([3, 4, 5, 6])
+    end
+
+    it "should be using a flattened array" do 
+      nums = SumAllCombinations.new([1,2,[3,4]])
+      nums.sum
+      expect(nums.calculated_values).to eq([3,4,5,5,6,6,7,7,8,9,10])
+    end
+  
+    it "should not remove duplicates by default" do
+      nums = SumAllCombinations.new([1,2,3,4])
+      nums.sum
+      expect(nums.calculated_values).to eq([3,4,5,5,6,6,7,7,8,9,10])
+    end
+
+    it "should remove duplicates is true passed in for remove_duplicates" do
+      nums = SumAllCombinations.new([1,2,3,4])
+      nums.sum(true)
+      expect(nums.calculated_values).to eq([3,4,5,6,7,8,9,10])
+    end
+   
+    it "should not sort if sort set to false" do
+      nums = SumAllCombinations.new([1,2,3,4])
+      nums.sum(true, false)
+      expect(nums.calculated_values).to eq([10, 6, 7, 3, 8, 4, 5, 9])
+    end
+ 
+    it "should set combinations used" do 
+      nums = SumAllCombinations.new([1,2,3])
+      nums.sum
+      expect(nums.combinations_used).to eq(["1+2+3=6", "1+2=3", "1+3=4", "2+3=5"])
+    end
+
+    it "should return empty array for combinations used if empty array is supplied" do
+      nums = SumAllCombinations.new([])
+      nums.sum
+      expect(nums.combinations_used).to eq([])
+    end
+
+    it "should return empty array for calculated value if empty array is supplied" do
+      nums = SumAllCombinations.new([])
+      nums.sum
+      expect(nums.calculated_values).to eq([])
+    end
+
+    it "should return empty array for calculated value if array is nil " do
+      nums = SumAllCombinations.new(nil)
+      nums.sum
+      expect(nums.calculated_values).to eq([])
+    end
+
+    it "should return empty array for combinations used if array is nill" do
+      nums = SumAllCombinations.new(nil)
+      nums.sum
+      expect(nums.combinations_used).to eq([])
+    end
+  end
+
 end
